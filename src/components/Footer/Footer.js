151ea37c17ad376473f49300339f9e88bd72a133
EnoshTs <enosh.tsur@haaretz.co.il>
2020-06-02 21:16:09 +0300

    react finish

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import classes from './Footer.module.css'
+import Radium from 'radium';
 
-export default function Footer() {
+function Footer() {
+
+    // state name
+    const [name, setName,] = React.useState('')
+
     return (
-        <div>
-            <h1>Hello I'm Footer!!!</h1>
+        <div className={classes.Footer}>
+            <input key="name-input" type="text" onChange={e => setName(e.target.value)} 
+                style={{
+                    ':focus': {
+                        border: '0.5rem solid red',
+                    }
+                }}
+            />
+            <h4 className={name.length > 3 ? classes.More : classes.Less}>
+                Hello {name}
+            </h4>
+            <h3 key="radium" style={{
+                color: 'purple',
+                ':hover': {
+                    color: 'blue',
+                    cursor: 'pointer'
+                }
+            }}>
+                With radium
+            </h3>
+            <h3 className={classes.Mijael}>Mijael</h3>
         </div>
     )
 }
 
+export default Radium(Footer)
 
 // rfc
 // react function component
